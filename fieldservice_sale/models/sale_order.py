0db5750386ff4d5a9591bb3c7b5adc69fc8be56a
Brian McMaster <brian@mcmpest.com>
2021-09-23 11:41:08 -0400

    [IMP] fsm_sale: Remove split invoice logic

--- fieldservice_sale/models/sale_order.py
+++ fieldservice_sale/models/sale_order.py
@@ -133,59 +133,6 @@ class SaleOrder(models.Model):
         return result
 
     @api.multi
-    def action_invoice_create(self, grouped=False, final=False):
-        invoice_ids = super().action_invoice_create(grouped, final)
-        result = invoice_ids or []
-
-        for invoice_id in invoice_ids:
-            invoice = self.env['account.invoice'].browse(invoice_id)
-            # check for invoice lines with product
-            # field_service_tracking = line
-            lines_by_line = self.env['account.invoice.line'].search([
-                ('invoice_id', '=', invoice_id),
-                ('product_id.field_service_tracking', '=', 'line')
-            ])
-            if len(lines_by_line) > 0:
-                line_count = len(invoice.invoice_line_ids)
-                for i in range(len(lines_by_line)):
-                    duplicate = True
-                    if ((i+1) == len(lines_by_line)) and \
-                       ((i+1) == line_count):
-                        duplicate = False
-                    inv = invoice
-                    if duplicate:
-                        inv = invoice.copy()
-                        inv.write({'invoice_line_ids': [(6, 0, [])]})
-                        inv.compute_taxes()
-                        invoice.compute_taxes()
-                        lines_by_line[i].invoice_id = inv.id
-                        result.append(inv.id)
-                    inv.fsm_order_ids = \
-                        [(4, lines_by_line[i].fsm_order_id.id)]
-            # check for invoice lines with product
-            # field_service_tracking = sale
-            lines_by_sale = self.env['account.invoice.line'].search([
-                ('invoice_id', '=', invoice_id),
-                ('product_id.field_service_tracking', '=', 'sale')
-            ])
-            if len(lines_by_sale) > 0:
-                fsm_orders = self.env['fsm.order'].search([
-                    ('sale_id', 'in', self.ids),
-                    ('sale_line_id', '=', False)
-                ])
-                if len(lines_by_sale) == len(invoice.invoice_line_ids):
-                    invoice.fsm_order_ids = \
-                        [(4, fsm_order.id) for fsm_order in fsm_orders]
-                elif len(invoice.invoice_line_ids) > len(lines_by_sale):
-                    new = invoice.copy()
-                    new.write({'invoice_line_ids': [(6, 0, [])]})
-                    lines_by_sale.write({'invoice_id': new.id})
-                    new.compute_taxes()
-                    invoice.compute_taxes()
-                    result.append(new.id)
-        return result
-
-    @api.multi
     def action_view_fsm_order(self):
         fsm_orders = self.mapped('fsm_order_ids')
         action = self.env.ref('fieldservice.action_fsm_dash_order').read()[0]
