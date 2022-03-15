350934996af6d21134ecf5ca99c401d83094cda4
Yves Goldberg <admin@ygol.com>
2022-03-15 09:28:55 +0200

    [FIX] fieldservice_project: adapt access to ir.actions.* records

--- fieldservice_project/models/project.py
+++ fieldservice_project/models/project.py
@@ -15,10 +15,10 @@ class Project(models.Model):
         This function returns an action that displays a full FSM Order
         form when creating an FSM Order from a project.
         """
-        action = self.env.ref(
-            "fieldservice.action_fsm_operation_order", raise_if_not_found=False
+        action = self.env["ir.actions.act_window"]._for_xml_id(
+            "fieldservice.action_fsm_operation_order"
         )
-        result = action.read()[0]
+        result = action
         # override the context to get rid of the default filtering
         result["context"] = {
             "default_project_id": self.id,
